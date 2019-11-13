1bf6d1b008facc01da89999a74dca3f433f03440
lukasso <lukasz.koladko@gmail.com>
2019-11-13 10:28:05 +0100

    grid base and html structure, gallery, offer boxes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import 'normalize.css';
 import './index.html'
 import './styles/style.scss';
 
-console.log('log test');
+
+const activeImage = document.querySelector(".product-image .active");
+const productImages = document.querySelectorAll(".image-list img");
+
+function changeImage(e) {
+    activeImage.src = e.target.src;
+}
+productImages.forEach(image => image.addEventListener("click", changeImage));
